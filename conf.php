3bee6a4072873912801e8e6d8f74e20e7b3d29b0
Bayu <rindonesiabayu@gmail.com>
2022-04-26 12:45:35 +0700

    first commit

---
+++ conf.php
@@ -0,0 +1,14 @@
+<?php
+
+# warna
+$clear = "\e[0m";
+$merah = "\e[31m";
+$hijau = "\e[32m";
+$kuning = "\e[33m";
+$cyan = "\e[36m";
+
+# jumlah akun berhasil login
+$jumlah = 0;
+
+# redirect jika berhasil login
+$successLogin = "http://portal.unimal.ac.id/index.php?pModule=ydKhmA==&pSub=ydKhmA==&pAct=18yZqg==";
